6a758434254d719812521c0d6b9679e58efd0e0b
Megha Dhabe <megha@faitco.com>
2026-07-30 10:41:38 -0500

    add PlayTest

---
+++ PlayTest/tests/seed.spec.ts
@@ -0,0 +1,9 @@
+// import { test, expect } from '@playwright/test';
+
+// test.describe('Test group', () => {
+//   test('seed', async ({ page }) => {
+//       await page.goto('https://playwright.dev/')
+  
+//       await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+//     });
+//   });
